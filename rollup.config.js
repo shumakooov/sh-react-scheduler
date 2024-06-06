6d272d821d92a554a04692ab6de700ec1937bc03
shumakooov <shumakov.13@yandex.ru>
2024-06-06 18:11:05 +0500

    (fix): improved appearance of components

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import replace from '@rollup/plugin-replace';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import external from "rollup-plugin-peer-deps-external";
 
-export default {
-    input: 'src/index.js',
-    output: {
-        file: 'lib/bundle.js',
-        format: 'iife'
+const packageJson = require("./package.json");
+
+export default [
+    {
+        input: "src/index.js",
+        output: [
+            { file: packageJson.main, format: "cjs", sourcemap: true },
+            { file: packageJson.module, format: "esm", sourcemap: true },
+        ],
+        plugins: [
+            resolve(),
+            commonjs({ include: ['node_modules/**'] }),
+            babel({
+                exclude: "node_modules/**",
+                presets: ["@babel/env", "@babel/preset-react"],
+                babelHelpers: 'bundled'
+            }),
+            postcss(),
+            external(),
+        ],
     },
-    plugins: [
-        nodeResolve({
-            extensions: ['.js', 'jsx']
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-react'],
-            extensions: ['.js']
-        }),
-        commonjs(),
-        replace({
-            preventAssignment: false,
-            'process.env.NODE_ENV': '"development"'
-        })
-    ]
-}
+];
